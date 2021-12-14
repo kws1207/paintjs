3e8919c964da0e89d31e60e1cb668f14feae822c
kws <kws1207@kaist.ac.kr>
2021-12-14 10:07:53 +0900

    fill color, save

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
+const save = document.getElementById('jsSave');
 
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = '#2c2c2c';
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = '#2c2c2c';
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
 	painting = false;
@@ -34,6 +41,7 @@ function onMouseMove(event) {
 function onColorClick(event) {
 	const color = event.target.style.backgroundColor;
 	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
 }
 
 function onRangeChange(event) {
@@ -41,15 +49,53 @@ function onRangeChange(event) {
 	ctx.lineWidth = size;
 }
 
+function onModeClick() {
+	if (filling === true) {
+		filling = false;
+		mode.innerText = 'Fill';
+	} else {
+		filling = true;
+		mode.innerText = 'Paint';
+	}
+}
+
+function onCanvasClick() {
+	if (filling) {
+		ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+	}
+}
+
+function handleCM(event) {
+	event.preventDefault();
+}
+
+function onSaveClick() {
+	const image = canvas.toDataURL('image/png');
+	const link = document.createElement('a');
+	link.href = image;
+	link.download = 'PantJS[🎨].png';
+	link.click();
+}
+
 if (canvas) {
 	canvas.addEventListener('mousemove', onMouseMove);
 	canvas.addEventListener('mousedown', startPainting);
 	canvas.addEventListener('mouseup', stopPainting);
 	canvas.addEventListener('mouseleave', stopPainting);
+	canvas.addEventListener('click', onCanvasClick);
+	canvas.addEventListener('contextmenu', handleCM);
 }
 
 Array.from(colors).forEach((color) => color.addEventListener('click', onColorClick));
 
 if (range) {
 	range.addEventListener('input', onRangeChange);
+}
+
+if (mode) {
+	mode.addEventListener('click', onModeClick);
+}
+
+if (save) {
+	save.addEventListener('click', onSaveClick);
 }
