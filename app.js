17afa4640211d2d5685e365eafd947a446dca30f
kws <kws1207@kaist.ac.kr>
2021-12-14 10:37:00 +0900

    clear

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const range = document.getElementById('jsRange');
 const mode = document.getElementById('jsMode');
+const clear = document.getElementById('jsClear');
 const save = document.getElementById('jsSave');
 
 const INITIAL_COLOR = '#2c2c2c';
@@ -12,7 +13,8 @@ canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
 ctx.strokeStyle = INITIAL_COLOR;
-ctx.fillStyle = INITIAL_COLOR;
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -77,6 +79,12 @@ function onSaveClick() {
 	link.click();
 }
 
+function onClearClick() {
+	ctx.strokeStyle = INITIAL_COLOR;
+	ctx.fillStyle = 'white';
+	ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
 if (canvas) {
 	canvas.addEventListener('mousemove', onMouseMove);
 	canvas.addEventListener('mousedown', startPainting);
@@ -94,6 +102,10 @@ if (range) {
 
 if (mode) {
 	mode.addEventListener('click', onModeClick);
+}
+
+if (clear) {
+	clear.addEventListener('click', onClearClick);
 }
 
 if (save) {
